181355a4932801b49be92a8a62f6119c3f412563
HoangHuy <jayker03212k5@gmail.com>
2026-03-14 17:57:42 +0700

    update

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,11 @@
-# Bước 1: Chọn môi trường chạy là Tomcat 9 và Java 8 (Khớp với cấu hình NetBeans của bạn)
+# Sử dụng Tomcat 9 và Java 8 (khớp với dự án của bạn)
 FROM tomcat:9.0-jdk8-openjdk
 
-# Bước 2: Dọn dẹp các ứng dụng mặc định để tránh xung đột
+# Xóa các ứng dụng mẫu để tránh nặng server
 RUN rm -rf /usr/local/tomcat/webapps/*
 
-# Bước 3: Copy file .war từ thư mục dist vào thư mục chạy của Tomcat
-# Chú ý: Đổi tên thành ROOT.war để web chạy trực tiếp tại đường dẫn gốc (/)
+# Copy file .war vào Tomcat và đổi tên thành ROOT.war để chạy ở trang chủ (/)
 COPY dist/ntXuanSystem.war /usr/local/tomcat/webapps/ROOT.war
 
-# Bước 4: Mở cổng 8080 để Render có thể truy cập
 EXPOSE 8080
-
-# Bước 5: Lệnh khởi động server
 CMD ["catalina.sh", "run"]
--- src/java/utils/DBUtils.java
+++ src/java/utils/DBUtils.java
@@ -4,34 +4,42 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-/**
- * DBUtils - Tiện ích kết nối cơ sở dữ liệu
- * 
- * ĐỂ THAY ĐỔI CẤU HÌNH KẾT NỐI:
- *   - Chỉnh DB_HOST nếu SQL Server không nằm trên localhost
- *   - Chỉnh USER_NAME / PASSWORD theo tài khoản SQL Server của bạn
- */
+
 public class DBUtils {
 
     // --- CẤU HÌNH KẾT NỐI ---
-    private static final String DB_HOST = "localhost";
-    private static final String DB_PORT = "1433";
-    private static final String DB_NAME = "DeliverySystemDB";
-    private static final String USER_NAME = "sa";
-    private static final String PASSWORD = "12345";
+//    private static final String DB_HOST = "localhost";
+////    private static final String DB_HOST = "db";
+//    private static final String DB_PORT = "1433";
+//    private static final String DB_NAME = "DeliverySystemDB";
+//    private static final String USER_NAME = "sa";
+//    private static final String PASSWORD = "YourStrongPassword123";
+//
+//    
+//    private static final String DB_URL =
+//        "jdbc:sqlserver://" + DB_HOST + ":" + DB_PORT
+//        + ";databaseName=" + DB_NAME
+//        + ";encrypt=true;trustServerCertificate=true;";
+//
+//    public static Connection getConnection() throws ClassNotFoundException, SQLException {
+//        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+//        return DriverManager.getConnection(DB_URL, USER_NAME, PASSWORD);
+//    }
+    
+    // Thay thông tin Aiven của bạn vào đây
+    private static final String DB_HOST = "delivery-db-mysql-jayker03212k5-ee32.f.aivencloud.com";
+    private static final String DB_PORT = "19281";
+    private static final String DB_NAME = "defaultdb";
+    private static final String USER_NAME = "avnadmin";
+    private static final String PASSWORD = "AVNS_txLmskApkmP4v1bHS0y";
 
-    // URL kết nối (encrypt+trustServerCertificate cho SQL Server 2019+)
+    // URL kết nối MySQL có bật SSL
     private static final String DB_URL =
-        "jdbc:sqlserver://" + DB_HOST + ":" + DB_PORT
-        + ";databaseName=" + DB_NAME
-        + ";encrypt=true;trustServerCertificate=true;";
+        "jdbc:mysql://" + DB_HOST + ":" + DB_PORT + "/" + DB_NAME + "?useSSL=true";
 
-    /**
-     * Lấy kết nối đến database.
-     * Ném ClassNotFoundException nếu thiếu driver sqljdbc4.jar
-     */
     public static Connection getConnection() throws ClassNotFoundException, SQLException {
-        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        // Đổi driver sang MySQL
+        Class.forName("com.mysql.cj.jdbc.Driver");
         return DriverManager.getConnection(DB_URL, USER_NAME, PASSWORD);
     }
 }
